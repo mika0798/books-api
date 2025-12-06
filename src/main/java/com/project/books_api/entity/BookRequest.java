181ffda8f1b94e28626c4f18f0820a557e4ddd74
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-06 12:06:58 +0700

    Add book request class for better update/create object validation

---
+++ src/main/java/com/project/books_api/entity/BookRequest.java
@@ -0,0 +1,68 @@
+package com.project.books_api.entity;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
+
+public class BookRequest {
+    @Size(min = 1, max = 50, message = "Title must be between 1 and 50 characters")
+    private String title;
+    @Size(min = 1, max = 30, message = "Author name must be between 1 and 30 characters")
+    private String author;
+    @Size(min = 1, max = 30, message = "Category must be between 1 and 30 characters")
+    private String category;
+    @Min(value = 1, message= "Rating must be larger than 0")
+    @Max(value = 5, message= "Rating cannot go pass 5")
+    private int rating;
+
+    public BookRequest() {
+    }
+
+    public BookRequest(String title, String author, String category, int rating) {
+        this.title = title;
+        this.author = author;
+        this.category = category;
+        this.rating = rating;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                ", category='" + category + '\'' +
+                '}';
+    }
+}
