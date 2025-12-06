8e6f464b2dd832b455a2de26edbca1439334da94
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-06 12:07:33 +0700

    Add handler for book exception

---
+++ src/main/java/com/project/books_api/controller/BookRestExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.project.books_api.controller;
+
+import com.project.books_api.exception.BookNotFoundException;
+import com.project.books_api.entity.BookErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class BookRestExceptionHandler {
+
+    @ExceptionHandler(BookNotFoundException.class)
+    public ResponseEntity<BookErrorResponse> handleBookNotFoundException(BookNotFoundException e) {
+
+        BookErrorResponse bookErrorResponse = new BookErrorResponse();
+
+        bookErrorResponse.setMessage(e.getMessage());
+        bookErrorResponse.setStatus(HttpStatus.NOT_FOUND.value());
+        bookErrorResponse.setTimestamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(bookErrorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<BookErrorResponse> handleException(Exception e) {
+
+        BookErrorResponse bookErrorResponse = new BookErrorResponse();
+
+        bookErrorResponse.setMessage(e.getMessage());
+        bookErrorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
+        bookErrorResponse.setTimestamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(bookErrorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+}
