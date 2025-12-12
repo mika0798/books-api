8b376b84968b465c6297d10e40d5d420bef6d4fb
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-12 16:27:39 +0700

    Switch to Lombok

--- src/main/java/com/project/books_api/entity/BookRequest.java
+++ src/main/java/com/project/books_api/entity/BookRequest.java
@@ -3,7 +3,13 @@ package com.project.books_api.entity;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookRequest {
     @Size(min = 1, max = 50, message = "Title must be between 1 and 50 characters")
     private String title;
@@ -18,54 +24,4 @@ public class BookRequest {
     @Max(value = 5, message= "Rating cannot go pass 5")
     private int rating;
 
-    public BookRequest() {
-    }
-
-    public BookRequest(String title, String author, String category, int rating) {
-        this.title = title;
-        this.author = author;
-        this.category = category;
-        this.rating = rating;
-    }
-
-    public int getRating() {
-        return rating;
-    }
-
-    public void setRating(int rating) {
-        this.rating = rating;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "author='" + author + '\'' +
-                ", title='" + title + '\'' +
-                ", category='" + category + '\'' +
-                '}';
-    }
 }
