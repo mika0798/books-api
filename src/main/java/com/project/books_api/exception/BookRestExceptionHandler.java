3a3c85187b5654eb3409ee3ee9be90cbdb4502f5
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-12 12:08:25 +0700

    Delete unnecessary package

--- src/main/java/com/project/books_api/exception/BookRestExceptionHandler.java
+++ src/main/java/com/project/books_api/exception/BookRestExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.project.books_api.exception;
 
+import com.project.books_api.entity.BookErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
