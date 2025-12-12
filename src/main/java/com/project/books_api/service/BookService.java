2aa2f0683b3afc9ee2ef322eddec3cb1755ac931
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-12 16:13:30 +0700

    update Book updating service

--- src/main/java/com/project/books_api/service/BookService.java
+++ src/main/java/com/project/books_api/service/BookService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookService {
@@ -20,12 +21,28 @@ public class BookService {
         return repository.findAll();
     }
 
-    public Book getBookById(Long bookId) {
-        return repository.findById(bookId).orElse(null);
+    public Optional<Book> getBookById(Long bookId) {
+        return repository.findById(bookId);
     }
 
     public Book saveBook(Book book) {
         return repository.save(book);
+    }
+
+    public Optional<Book> updateBook(Long id, Book book) {
+        Optional<Book> optionalBook = repository.findById(id);
+        if (optionalBook.isPresent()) {
+            Book updatedBook = optionalBook.get();
+
+            updatedBook.setTitle(book.getTitle());
+            updatedBook.setAuthor(book.getAuthor());
+            updatedBook.setCategory(book.getCategory());
+            updatedBook.setRating(book.getRating());
+
+            return Optional.of(repository.save(book));
+        }
+
+        return Optional.empty();
     }
 
     public void deleteBookById(Long bookId) {
