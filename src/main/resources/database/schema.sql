7921a28234bf6b95b3a8861fbc462d8541633677
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-12 20:14:17 +0700

    Add schema and data scripts

--- src/main/resources/database/schema.sql
+++ src/main/resources/database/schema.sql
@@ -1,8 +1,9 @@
-CREATE TABLE IF NOT EXISTS books {
+USE books_api;
+CREATE TABLE IF NOT EXISTS books (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(100),
     author VARCHAR(100),
     category VARCHAR(100),
     rating INT,
     CONSTRAINT CHK_rating CHECK (rating BETWEEN 1 AND 5)
-}
+);
