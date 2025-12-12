82d70865d17830447807d62fb5642309875d7e1e
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-12 20:15:54 +0700

    Add interface to reduce tight coupling

--- src/main/java/com/project/books_api/service/BookService.java
+++ src/main/java/com/project/books_api/service/BookService.java
@@ -1,52 +1,15 @@
 package com.project.books_api.service;
 
-import com.project.books_api.dao.BookRepository;
 import com.project.books_api.entity.Book;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
-public class BookService {
-    private BookRepository repository;
-
-    @Autowired
-    public void setRepository(BookRepository repository) {
-        this.repository = repository;
-    }
-
-    public List<Book> getAllBooks() {
-        return repository.findAll();
-    }
-
-    public Optional<Book> getBookById(Long bookId) {
-        return repository.findById(bookId);
-    }
-
-    public Book saveBook(Book book) {
-        return repository.save(book);
-    }
-
-    public Optional<Book> updateBook(Long id, Book book) {
-        Optional<Book> optionalBook = repository.findById(id);
-        if (optionalBook.isPresent()) {
-            Book updatedBook = optionalBook.get();
-
-            updatedBook.setTitle(book.getTitle());
-            updatedBook.setAuthor(book.getAuthor());
-            updatedBook.setCategory(book.getCategory());
-            updatedBook.setRating(book.getRating());
-
-            return Optional.of(repository.save(book));
-        }
-
-        return Optional.empty();
-    }
-
-    public void deleteBookById(Long bookId) {
-        repository.deleteById(bookId);
-    }
-
+public interface BookService {
+    List<Book> getAllBooks();
+    Optional<Book> getBookById(Long bookId);
+    List<Book> getBooksByCategory(String category);
+    Book saveBook(Book book);
+    Optional<Book> updateBook(Long id, Book book);
+    void deleteBookById(Long bookId);
 }
