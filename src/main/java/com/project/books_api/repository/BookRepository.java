5199aa7731302a637d5344b5fd148a065d45044f
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-12 20:15:15 +0700

    Add custom derived query

--- src/main/java/com/project/books_api/repository/BookRepository.java
+++ src/main/java/com/project/books_api/repository/BookRepository.java
@@ -1,9 +1,11 @@
-package com.project.books_api.dao;
+package com.project.books_api.repository;
 
 import com.project.books_api.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.List;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findByCategoryIgnoreCase(String category);
 }
