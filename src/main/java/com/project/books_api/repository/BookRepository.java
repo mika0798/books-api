dfc8cb01ba05dfc1e277a7c76d7e94cec5716341
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-13 08:10:25 +0700

    Add Optional<> as return type

--- src/main/java/com/project/books_api/repository/BookRepository.java
+++ src/main/java/com/project/books_api/repository/BookRepository.java
@@ -5,7 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+    Optional<Book> getBookById(Long bookId);
     List<Book> findByCategoryIgnoreCase(String category);
 }
