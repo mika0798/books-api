96fc22756364a0fe1479d6ccbb5d3982d054662e
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-19 16:02:51 +0700

    Fix wrong return object

--- src/main/java/com/project/books_api/validator/BookValidator.java
+++ src/main/java/com/project/books_api/validator/BookValidator.java
@@ -1,20 +1,23 @@
 package com.project.books_api.validator;
 
-import com.project.books_api.entity.Book;
+import com.project.books_api.dto.BookRequest;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
+
+
+// ===== For practicing making custom validator =====
 
 public class BookValidator implements Validator {
 
     @Override
     public boolean supports(Class<?> clazz) {
-        return Book.class.equals(clazz);
+        return BookRequest.class.equals(clazz);
     }
 
     @Override
     public void validate(Object target, Errors errors) {
-        Book book = (Book) target;
+        BookRequest book = (BookRequest) target;
 
         if (book.getPrice() < 1) {
             errors.rejectValue("price","price.invalid", "price must be greater than 0");
