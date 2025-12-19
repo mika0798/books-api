5a3581fb4a19bd3de9ad3cb23ea0509072ac32c1
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-19 16:18:39 +0700

    Handling exception for validation error

--- src/main/java/com/project/books_api/controller/BookRestExceptionHandler.java
+++ src/main/java/com/project/books_api/controller/BookRestExceptionHandler.java
@@ -1,11 +1,16 @@
 package com.project.books_api.controller;
 
 import com.project.books_api.dto.BookErrorResponse;
+import com.project.books_api.dto.BookValidationErrorResponse;
 import com.project.books_api.exception.BookNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class BookRestExceptionHandler {
@@ -34,4 +39,21 @@ public class BookRestExceptionHandler {
         return new ResponseEntity<>(bookErrorResponse, HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<BookValidationErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException exc) {
+        Map<String,String> errors = new HashMap<>();
+
+                exc
+                .getBindingResult()
+                .getFieldErrors()
+                .forEach(error -> errors.put(error.getField(),error.getDefaultMessage()));
+
+                BookValidationErrorResponse bookValidationErrorResponse = new BookValidationErrorResponse(
+                        HttpStatus.BAD_REQUEST.value(),
+                        "Validation failed",
+                        System.currentTimeMillis(),
+                        errors
+                );
+                return new ResponseEntity<>(bookValidationErrorResponse, HttpStatus.BAD_REQUEST);
+    }
 }
