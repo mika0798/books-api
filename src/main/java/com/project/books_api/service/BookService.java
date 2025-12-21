2f94e7e7200457b875dc0afdd21982c22fc89deb
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-21 23:36:06 +0700

    Add two patch and convert methods to the interface

--- src/main/java/com/project/books_api/service/BookService.java
+++ src/main/java/com/project/books_api/service/BookService.java
@@ -1,8 +1,10 @@
 package com.project.books_api.service;
 
+import com.project.books_api.dto.BookRequest;
 import com.project.books_api.entity.Book;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public interface BookService {
@@ -11,4 +13,6 @@ public interface BookService {
     List<Book> getBooksByCategory(String category);
     Book saveBook(Book book);
     void deleteBookById(Long bookId);
+    Book convertBook(long id, BookRequest bookRequest);
+    Book patchBook(Map<String,Object> patchPayload, Book book);
 }
