7d8226980921d80b4c3463fdfef21dc5dafbedc3
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-22 10:18:13 +0700

    Remove unused method

--- src/main/java/com/project/books_api/repository/BookRepository.java
+++ src/main/java/com/project/books_api/repository/BookRepository.java
@@ -8,6 +8,5 @@ import java.util.List;
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-    Optional<Book> getBookById(Long bookId);
     List<Book> findByCategoryIgnoreCase(String category);
 }
