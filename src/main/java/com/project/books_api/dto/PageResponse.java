86032c078bed552e36aff84d4ec77512062f1bd9
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-22 10:37:04 +0700

    Add PageResponse record class

---
+++ src/main/java/com/project/books_api/dto/PageResponse.java
@@ -0,0 +1,11 @@
+package com.project.books_api.dto;
+
+import java.util.List;
+
+public record PageResponse<T>(
+        List<T> content,
+        int page,
+        int size,
+        long totalElements,
+        int totalPages
+) {}
