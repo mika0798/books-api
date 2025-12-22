af9cbe70d5c8a3207ec05caf68480348a00d6ca5
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-22 10:38:46 +0700

    Add pagination method

--- src/main/java/com/project/books_api/repository/BookRepository.java
+++ src/main/java/com/project/books_api/repository/BookRepository.java
@@ -1,12 +1,14 @@
 package com.project.books_api.repository;
 
 import com.project.books_api.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     List<Book> findByCategoryIgnoreCase(String category);
+    Page<Book> findByCategoryIgnoreCase(String category, Pageable pageable);
 }
--- src/main/java/com/project/books_api/service/BookService.java
+++ src/main/java/com/project/books_api/service/BookService.java
@@ -2,15 +2,17 @@ package com.project.books_api.service;
 
 import com.project.books_api.dto.BookRequest;
 import com.project.books_api.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 public interface BookService {
     List<Book> getAllBooks();
     Book getBookById(Long bookId);
     List<Book> getBooksByCategory(String category);
+    Page<Book> getBooksByCategory(String category, Pageable pageable);
     Book saveBook(Book book);
     void deleteBookById(Long bookId);
     Book convertBook(long id, BookRequest bookRequest);
